0437e961390a38e0041d180c453348e0340acd54
alicebobscorpio <alicebobscorpio@gmail.com>
2023-11-20 22:39:22 +0800

    define the API framework

---
+++ api/README.md
@@ -0,0 +1,3 @@
+The users shall import only package pqringctx.api to call the functions of pqringctx.
+The exported data structures will be used by both pqringctx, pqringctx, and the caller, 
+so they are defined in package pqringctx.
---
+++ api/api.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"github.com/cryptosuite/pqringctx"
+	"github.com/cryptosuite/pqringctx/apidao"
+)
+
+// AddressKeyGenForRing generates coinAddress, coinSpendKey, and coinSnKey
+// for the key which will be used to host the coins with full-privacy.
+// Note that keys are purely in cryptography, we export bytes,
+// and packages the cryptographic details in pqringctx.
+func AddressKeyGenForRing(pp *pqringctx.PublicParameter, seed []byte) (coinAddress []byte, coinSpendKey []byte, coinSnKey []byte, err error) {
+	return nil, nil, nil, err
+}
+
+// AddressKeyGenForSingle generates coinAddress and coinSpendKey
+// for the key which will be used to host the coins with pseudonym-privacy.
+// Note that keys are purely in cryptography, we export bytes,
+// and packages the cryptographic details in pqringctx.
+func AddressKeyGenForSingle(pp *pqringctx.PublicParameter, seed []byte) (coinAddress []byte, coinSpendKey []byte, err error) {
+	return nil, nil, err
+}
+
+// ValueKeyGen generates serializedValuePublicKey and serializedValueSecretKey,
+// which will be used to transmit the (value, randomness) pair of the value-commitment to the coin owner.
+// Note that by default, pqringctx transmits the (value, randomness) pair by on-chain data,
+// i.e., the ciphertexts are included in Txo.
+// As the encryption/transmit of (value, randomness) pair is independent from the coinAddress part,
+// we use a standalone ValueKeyGen algorithm to generate these keys.
+func ValueKeyGen(pp *pqringctx.PublicParameter, seed []byte) (serializedValuePublicKey []byte, serializedValueSecretKey []byte, err error) {
+	return nil, nil, err
+}
+
+// CoinbaseTxGen generates CoinbaseTx.
+// As the caller may decompose the components of the generated CoinbaseTx
+// to make a chain-layer transaction,
+// CoinbaseTxGen outputs an apidao.CoinbaseTxMLP, rather than a serialized Tx.
+func CoinbaseTxGen(pp *pqringctx.PublicParameter, vin uint64, txOutputDescs []*apidao.TxOutputDescMLP, txMemo []byte) (cbTx *apidao.CoinbaseTxMLP, err error) {
+	return nil, err
+}
+
+func CoinbaseTxVerify(pp *pqringctx.PublicParameter, cbTx *apidao.CoinbaseTxMLP) (bool, error) {
+	return false, nil
+}
+
+// TransferTxGen generates TransferTx.
+// As the caller may decompose the components of the generated TransferTx
+// to make a chain-layer transaction,
+// TransferTxGen outputs a apidao.TransferTxMLP, rather than a serialized Tx.
+func TransferTxGen(pp *pqringctx.PublicParameter, inputDescs []*apidao.TxInputDescMLP, outputDescs []*apidao.TxOutputDescMLP, fee uint64, txMemo []byte) (trTx *apidao.TransferTxMLP, err error) {
+	return nil, err
+}
+
+func TransferTxVerify(pp *pqringctx.PublicParameter, trTx *apidao.TransferTxMLP) (bool, error) {
+	return false, nil
+}
