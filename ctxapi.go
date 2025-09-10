5db76aa349babea939428ac4cc64fe9dd138f5b4
alicebobscorpio <alicebobscorpio@gmail.com>
2025-09-10 10:01:01 +0800

    Create ctxapi.go
    add constants for ctx

---
+++ ctxapi.go
@@ -0,0 +1,9 @@
+package pqringctx
+
+// CTX
+const (
+	MaxAllowedCtxTxoSize           uint32 = 1048576  // 2^20
+	MaxAllowedCtxTxWitnessCbTxSize uint32 = 8388608  // 2^23, 8M bytes
+	MaxAllowedCtxTxWitnessTrTxSize uint32 = 16777216 // 2^24, 16M bytes
+
+)
