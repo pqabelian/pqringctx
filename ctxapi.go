beb267367edc42416767b3b0ad2e90bd3b64190a
alicebobscorpio <alicebobscorpio@gmail.com>
2025-12-21 09:28:25 +0800

    Update ctxapi.go
    review done

--- ctxapi.go
+++ ctxapi.go
@@ -2,8 +2,10 @@ package pqringctx
 
 // CTX
 const (
-	MaxAllowedCtxTxoSize           uint32 = 1048576  // 2^20, 1M bytes
-	MaxAllowedCtxTxWitnessCbTxSize uint32 = 8388608  // 2^23, 8M bytes
-	MaxAllowedCtxTxWitnessTrTxSize uint32 = 16777216 // 2^24, 16M bytes
+	MaxAllowedCtxTxoSize           uint32 = 1024 * 1024      // 2^20, 1M bytes
+	MaxAllowedCtxTxWitnessCbTxSize uint32 = 8 * 1024 * 1024  // 2^23, 8M bytes
+	MaxAllowedCtxTxWitnessTrTxSize uint32 = 16 * 1024 * 1024 // 2^24, 16M bytes
 
 )
+
+// review done 2025.12.21
